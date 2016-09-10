b00f67496e71eb34c21c2fdeb599971c89bf1d1d
Jayshree Anandakumar <jayshreeanand90@gmail.com>
2016-09-10 21:19:06 +0530

    Configure carrierwave

---
+++ config/initializers/carrier_wave.rb
@@ -0,0 +1,24 @@
+require 'carrierwave/orm/activerecord'
+
+CarrierWave.configure do |config|
+  if Rails.env.production? || Rails.env.staging?
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: Rails.application.secrets.aws_access_key_id,
+      aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
+      persistent: false,
+      region: Rails.application.secrets.fog_region
+    }
+    config.fog_directory = Rails.application.secrets.fog_directory
+    config.storage = :fog
+    config.remove_previously_stored_files_after_update = true
+  else
+    config.storage = :file
+  end
+
+  config.asset_host = ENV['ASSET_HOST']
+
+  if Rails.env.test?
+    config.enable_processing = false
+  end
+end
