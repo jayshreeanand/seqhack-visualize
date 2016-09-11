dca875104f7252670692b743cad681fa3bbb3986
Jayshree Anandakumar <jayshreeanand90@gmail.com>
2016-09-11 07:43:47 +0530

    Typo fix

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -12,7 +12,7 @@ CarrierWave.configure do |config|
     config.fog_directory = Rails.application.secrets.fog_directory
     config.storage = :fog
     config.remove_previously_stored_files_after_update = true
-    config.asset_host = "https://s3.amazonaws.com/#{Rails.applicaiton.secrets.fog_directory}"
+    config.asset_host = "https://s3.amazonaws.com/#{Rails.application.secrets.fog_directory}"
   else
     config.storage = :file
   end
