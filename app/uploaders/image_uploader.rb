4d370b49ab4c48ad0ae53ff06fbd4a0e8b892843
Jayshree Anandakumar <jayshreeanand90@gmail.com>
2016-09-11 08:20:20 +0530

    Active admin jcrop for carrierwave

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,12 +1,14 @@
 # encoding: utf-8
 class ImageUploader < BaseUploader
   include CarrierWave::RMagick
+  include ActiveAdminJcrop::AssetEngine::CarrierWave
 
   def extension_white_list
     %w(jpg jpeg gif png)
   end
 
   version :thumb do
+    process :active_admin_crop
     process resize_to_fit: [100, 100]
   end
 end
